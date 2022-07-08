9aea3830ab5c9670a9f8f675ee4f9c3e78761667
Danya <theincredibledanzel@yandex.ru>
2022-07-08 16:36:40 +0400

    complete 2 variant

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -11,54 +11,63 @@ void FillArraySpiral(int [,] a)
     int endCellY = a.GetLength(0) / 2;
     int endCellX = a.GetLength(1) / 2 - 1;
     //Console.WriteLine("Test " + endCellY + ", " + endCellX);
+    //Console.WriteLine();
     int yReducingLength = a.GetLength(0) - 1;
     int xReducingLength = a.GetLength(1) - 1;
     int yReducingLengthReverse = 0;
     int xReducingLengthReverse = 0;
     int y = 0;
     int x = 0;
+    int yCheck = y;
+    int xCheck = x;
     int guideArrow = 0;
+    int guideArrowRelay = 0;
     a[0, 0] = number;
     //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
     number++;
-    while(y != endCellY || x != endCellX)
+    while(yCheck != endCellY || xCheck != endCellX)
     {
-        //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
+        guideArrow = guideArrowRelay;
+        yCheck = y;
+        //Console.WriteLine("Test Check " + yCheck + ", " + xCheck);
         if(guideArrow == 0)
         {
             x++;
+            xCheck = x;
             a[y, x] = number;
             //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
             number++;
             if(x == xReducingLength)
             {
-                guideArrow++;
+                guideArrowRelay++;
             }
         }
         if(guideArrow == 1)
         {
             y++;
+            yCheck = y;
             a[y, x] = number;
             //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
             number++;
             if(y == yReducingLength)
             {
-                guideArrow++;
+                guideArrowRelay++;
             }
         }
         if(guideArrow == 2)
         {
             x--;
+            xCheck = x;
             a[y, x] = number;
             //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
-            if(y == endCellY && x == endCellX)
+            /*if(y == endCellY && x == endCellX)
             {
                 break;
-            }
+            }*/
             number++;
             if(x == xReducingLengthReverse)
             {
-                guideArrow++;
+                guideArrowRelay++;
                 y--;
             }
         }
@@ -68,10 +77,12 @@ void FillArraySpiral(int [,] a)
             //Console.WriteLine("Test " + a[y, x] + " [" + y + ", " + x + "]");
             number++;
             y--;
+            yCheck = y;
             if(y == yReducingLengthReverse)
             {
-                guideArrow = 0;
+                guideArrowRelay = 0;
                 y++;
+                yCheck = y;
                 yReducingLength--;
                 xReducingLength--;
                 yReducingLengthReverse++;
